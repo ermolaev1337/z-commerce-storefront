28d6d316421276c95899b313c134da2f4fa61448
Egor Ermolaev <egor.ermolaev@uni.lu>
2024-02-06 16:02:30 +0100

    remove(shipping): remove shipping address input fields

--- src/modules/checkout/components/shipping-address/index.tsx
+++ src/modules/checkout/components/shipping-address/index.tsx
@@ -9,14 +9,10 @@ import { Container } from "@medusajs/ui"
 const ShippingAddress = ({
   customer,
   cart,
-  checked,
-  onChange,
   countryCode,
 }: {
   customer: Omit<Customer, "password_hash"> | null
   cart: Omit<Cart, "refundable_amount" | "refunded_total"> | null
-  checked: boolean
-  onChange: () => void
   countryCode: string
 }) => {
   const [formData, setFormData] = useState({
@@ -85,94 +81,23 @@ const ShippingAddress = ({
         </Container>
       )}
       <div className="grid grid-cols-2 gap-4">
-        <Input
-          label="First name"
-          name="shipping_address.first_name"
-          autoComplete="given-name"
-          value={formData["shipping_address.first_name"]}
-          onChange={handleChange}
-          required
-        />
-        <Input
-          label="Last name"
-          name="shipping_address.last_name"
-          autoComplete="family-name"
-          value={formData["shipping_address.last_name"]}
-          onChange={handleChange}
-          required
-        />
-        <Input
-          label="Address"
-          name="shipping_address.address_1"
-          autoComplete="address-line1"
-          value={formData["shipping_address.address_1"]}
-          onChange={handleChange}
-          required
-        />
-        <Input
-          label="Company"
-          name="shipping_address.company"
-          value={formData["shipping_address.company"]}
-          onChange={handleChange}
-          autoComplete="organization"
-        />
-        <Input
-          label="Postal code"
-          name="shipping_address.postal_code"
-          autoComplete="postal-code"
-          value={formData["shipping_address.postal_code"]}
-          onChange={handleChange}
-          required
-        />
-        <Input
-          label="City"
-          name="shipping_address.city"
-          autoComplete="address-level2"
-          value={formData["shipping_address.city"]}
-          onChange={handleChange}
-          required
-        />
         <CountrySelect
           name="shipping_address.country_code"
           autoComplete="country"
           region={cart?.region}
           value={formData["shipping_address.country_code"]}
           onChange={handleChange}
-          required
+
         />
         <Input
-          label="State / Province"
-          name="shipping_address.province"
-          autoComplete="address-level1"
-          value={formData["shipping_address.province"]}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="my-8">
-        <Checkbox
-          label="Same as billing address"
-          name="same_as_billing"
-          checked={checked}
-          onChange={onChange}
-        />
-      </div>
-      <div className="grid grid-cols-2 gap-4 mb-4">
-        <Input
-          label="Email"
-          name="email"
-          type="email"
-          title="Enter a valid email address."
-          autoComplete="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <Input
-          label="Phone"
-          name="shipping_address.phone"
-          autoComplete="tel"
-          value={formData["shipping_address.phone"]}
-          onChange={handleChange}
+            label="Email"
+            name="email"
+            type="email"
+            title="Enter a valid email address."
+            autoComplete="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
         />
       </div>
     </>
